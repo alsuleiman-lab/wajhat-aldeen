f90014922be036cac5c64219a93141f113ab675f
alsuleiman-lab <alsuleiman.de@gmail.com>
2026-06-06 15:32:06 +0200

    add android/app/src/main/kotlin/com/wajhat/aldeen/IslamicWidgetReceiver.kt

---
+++ android/app/src/main/kotlin/com/wajhat/aldeen/IslamicWidgetReceiver.kt
@@ -0,0 +1,30 @@
+package com.wajhat.aldeen
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import es.antonborri.home_widget.HomeWidgetPlugin
+
+class IslamicWidgetReceiver : AppWidgetProvider() {
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    private fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
+        val widgetData = HomeWidgetPlugin.getData(context)
+        val views = RemoteViews(context.packageName, R.layout.glance_default_loading_layout)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+}
